7f6f43b53d505772f6fdbbf2f6a69c9b5c2b53ce
crispipear <rice74@uw.edu>
2018-11-18 19:01:41 -0800

    Updates

---
+++ precache-manifest.d68b791f5da86b25462a2922c2f17400.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9c17d2a89935730116e7",
+    "url": "/static/js/main.9c17d2a8.chunk.js"
+  },
+  {
+    "revision": "45eed07ed99d96335327",
+    "url": "/static/js/1.45eed07e.chunk.js"
+  },
+  {
+    "revision": "9c17d2a89935730116e7",
+    "url": "/static/css/main.97ba88ed.chunk.css"
+  },
+  {
+    "revision": "52ab84fa498f61c18e8b777f43495f12",
+    "url": "/index.html"
+  }
+];
