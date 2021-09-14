e4bd8bef73790854703a46fb2e31caa0db1fd159
crispipear <lisuy97@gmail.com>
2021-09-13 19:57:46 -0700

    Updates

---
+++ precache-manifest.a477cfe9c1c473114d3f18bdeb181fcc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "74a0e9462da39bb5aaa3",
+    "url": "/uwbimd2019.com/static/css/main.f73dd097.chunk.css"
+  },
+  {
+    "revision": "74a0e9462da39bb5aaa3",
+    "url": "/uwbimd2019.com/static/js/main.74a0e946.chunk.js"
+  },
+  {
+    "revision": "83258d3dea99fdceab58",
+    "url": "/uwbimd2019.com/static/js/1.83258d3d.chunk.js"
+  },
+  {
+    "revision": "e8197cbb2639f47062be",
+    "url": "/uwbimd2019.com/static/js/runtime~main.e8197cbb.js"
+  },
+  {
+    "revision": "7a4bc01c602ef7d61cb34db71a9b1147",
+    "url": "/uwbimd2019.com/static/media/texture.7a4bc01c.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/uwbimd2019.com/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6bbbbadf8cdf21ea495c39966558a4fb",
+    "url": "/uwbimd2019.com/static/media/linkedin.6bbbbadf.svg"
+  },
+  {
+    "revision": "69d91db968a6799af44b6c13fdbd74d0",
+    "url": "/uwbimd2019.com/static/media/link.69d91db9.svg"
+  },
+  {
+    "revision": "5af34745b5dc734bf10de77f0ff7375a",
+    "url": "/uwbimd2019.com/static/media/facebook.5af34745.svg"
+  },
+  {
+    "revision": "03892e3f26c5cd4ddb493a7953c5907c",
+    "url": "/uwbimd2019.com/static/media/twitter.03892e3f.svg"
+  },
+  {
+    "revision": "0efcc600c00e75f060d3a9b8104eaeb6",
+    "url": "/uwbimd2019.com/static/media/patterns.0efcc600.svg"
+  },
+  {
+    "revision": "6c8df42e43023cf42af16a4acaf2697a",
+    "url": "/uwbimd2019.com/static/media/patterns_mobile.6c8df42e.svg"
+  },
+  {
+    "revision": "87eb6179b1517573ff5525eb8d6ec297",
+    "url": "/uwbimd2019.com/static/media/hourglass.87eb6179.svg"
+  },
+  {
+    "revision": "eab7579ac13ffe13955cee3b20698e92",
+    "url": "/uwbimd2019.com/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a9d7ccf38e8ee714268f2df0ccc5fcc6.js"
+  "/uwbimd2019.com/precache-manifest.a477cfe9c1c473114d3f18bdeb181fcc.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/uwbimd2019.com/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
